daf047a0f46f823adf7009ad395b6ec3c9388fb3
Anna Nguyen <xngu8505@uni.sydney.edu.au>
2024-02-25 23:09:17 +1100

    Changed from class components to functional components. Successfully displays map and reads from the JSON file to display multiple markers on the map

--- mappapp/src/App.js
+++ mappapp/src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
-import MapApp from './Map';
+import { Map } from './Map';
+import './App.css';
 
-class App extends Component {
 
-  render() {
-    return (
-      <MapApp />
-    );
-  }
-
+export default function App() {
+  return (
+    <Map />
+  );
 }
 
-export default App;
+// x="-33.87880901103003" y="151.20603254341876" 
--- mappapp/src/Map.js
+++ mappapp/src/Map.js
@@ -1,25 +1,49 @@
-import React, { Component } from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import React from 'react';
+import { MapContainer, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import './Map.css';
+import data from './data.json';
 
-// const center = [-33.87880901103003, 151.20603254341876];
-  
-class MapApp extends Component {
+let pinIcon = L.icon({
+    iconUrl: icon,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+});
 
-    // center = [-33.87880901103003, 151.20603254341876];
+function GetMap() {
+    const map = useMap();
+    map.setView([-33.87880901103003, 151.20603254341876], 13);
+    L.tileLayer('https://api.maptiler.com/maps/topo-v2/256/{z}/{x}/{y}.png?key=NylqWfM0CVoaz9pFVfG0', {
+        attribution:'<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+    }).addTo(map);
 
-    render() {
-        return (
-            <MapContainer 
-                center={[-33.87880901103003, 151.20603254341876]} 
-                zoom={10} 
-                style={{width: '100vw', height: '100vh'}}>
-            <TileLayer 
-                url="https://api.maptiler.com/maps/topo-v2/256/{z}/{x}/{y}.png?key=NylqWfM0CVoaz9pFVfG0"
-                attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'/>
-            </MapContainer>
-        );
+    for (let i = 0; i < data.length; i++) {
+        L.marker([data[i].latitude, data[i].longitude], {
+            icon: pinIcon
+        }).addTo(map);
     }
+
+    return null;
 }
 
-export default MapApp;
+export function Map() {
+    return (
+        <div>
+            <MapContainer id="map">
+                <GetMap />
+            </MapContainer>
+        </div>
+    );
+}
+
+//     <MapContainer 
+//         center={[-33.87880901103003, 151.20603254341876]} 
+//         zoom={13} 
+//         style={{width: '100vw', height: '100vh'}}>
+//     <TileLayer 
+//         url="https://api.maptiler.com/maps/topo-v2/256/{z}/{x}/{y}.png?key=NylqWfM0CVoaz9pFVfG0"
+//         attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'/>
+//     <Marker position={[-33.87880901103003, 151.20603254341876]} icon={pinIcon} />
+//     </MapContainer>
